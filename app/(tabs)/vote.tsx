e9d21ebc619f2916628b8c7c3b1a5b9c892710f6
captainteodor <93284470+captainteodor@users.noreply.github.com>
2025-07-13 16:58:06 +0300

    asdf

--- app/(tabs)/vote.tsx
+++
@@ -1,6 +0,0 @@
-// app/(tabs)/voting.tsx
-import VotingScreen from '../../components/VotingScreen'; // Adjust the path based on your structure
-
-export default function VotingTab() {
-  return <VotingScreen />;
-}
